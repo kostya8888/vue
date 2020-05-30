1a0e8489f0dda0a2d7fb772966c597cd1d05830f
kostya8888 <reii@list.ru>
2020-05-30 10:30:24 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,44 @@
-<script>
-console.log('111111111111111111111s555555s dse ')
-console.log('sdss')
-<script>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My first Vue app</title>
+  <script src="vue.js"></script>
+</head>
+<body>
+  <div id="app">
+    <input id="input" v-on:input="val = $event.target.value" type="text" />
+    <button v-on:click="addItem">кнопка</button>
+  <ul id="todo_lis">     
+      
+  </ul>
+  <ul>
+    <li v-on:click="checked" v-for="(item, index) in lis">{{index}} - {{item}}</li>
+  </ul>
+  </div>
+
+  <script>
+    var app = new Vue({
+      el: '#app',
+      data: {
+          val: 'empty string',
+          lis: ['list1', 'list2', 'list3']
+      },
+      methods: {
+        addItem (){
+          let div = document.createElement('li');
+            div.innerHTML = '<label ><input type="checkbox" />' + this.val + '</label>';
+            document.getElementById("todo_lis").appendChild(div);           
+            document.getElementById("input").value = "";
+            div.setAttribute('@click', 'checked')
+        },
+        checked (e){          
+          e.target.classList.toggle("checked")
+        }
+      }
+    })
+  </script>
+  <style>
+  	.checked { text-decoration: line-through;}
+  </style>
+</body>
+</html>
